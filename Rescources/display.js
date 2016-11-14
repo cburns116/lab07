3447ba9c9f20f2cf01861993c1e531250e1840ac
Corey Burns <Corey Burns>
2016-11-14 18:00:47 -0500

    Added comments to the code so TA and Prof can understand

--- Rescources/display.js
+++ Rescources/display.js
@@ -8,30 +8,33 @@ $(document).ready(function(){
             var $xml = $(xml);
             
 			console.log($xml);
-			
+			//find each homework assignments values from xml
 			$xml.find('homework').each(function () {
 				var $this = $(this),
 					name = $this.find('assignment').text(),
 					link = $this.find('solution').text(),
 					desc = $this.find('description').text();
+                    //insert them into the display data div
                 $("#displaydata").append(
 					"<h3><a href='" + link + "'>" + name + "</a></h3><div><p><a href='" + link + "'title='Solution to " + name + "''>" + desc + "</a></p></div>"
 				);
             });
-			
+			//find each lab assignment values from xml
 			$xml.find('lab').each(function () {
                 var $this = $(this),
                     name = $this.find('assignment').text(),
                     link = $this.find('solution').text(),
                     desc = $this.find('description').text();
+                    //insert them into the display data div
                 $("#displaydata").append(
                     "<h3><a href='" + link + "'>" + name + "</a></h3><div><p><a href='" + link + "'title='Solution to " + name + "''>" + desc + "</a></p></div>"
             );
             });
-
+            //uses the acordion class to create the jquery ui
             $( function() {
                 $( ".accordion" ).accordion();
             } );
+            //by adding titles above tooltip displays them when hovering
             $( function() {
                 $( document ).tooltip();
             } );
